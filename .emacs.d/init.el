9c42630023401c4ac4cad6cf7c2837394ce4a8cc
Atsushi Yoshinari <a.bit.of.elbowroomer@gmail.com>
2017-05-06 23:55:45 +0900

    init.el を追加した

---
+++ .emacs.d/init.el
@@ -0,0 +1,33 @@
+(when load-file-name
+  (setq user-emacs-directory (file-name-directory load-file-name)))
+
+(add-to-list 'load-path (locate-user-emacs-file "el-get/el-get"))
+
+(unless (require 'el-get nil 'noerror)
+  (require 'package)
+  (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+  (add-to-list 'package-pinned-packages '((el-get . "melpa")))
+  (package-refresh-contents)
+  (package-initialize)
+  (package-install 'el-get)
+  (require 'el-get))
+
+(add-to-list 'el-get-recipe-path (locate-user-emacs-file "el-get-user/recipes"))
+
+(el-get-bundle! 'init-loader
+  (setq-default init-loader-show-log-after-init nil)
+  (init-loader-load))
+
+(el-get 'sync)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (anything init-loader))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
